c0308fb2860da62303ee2a2670c7fd737bde358f
Rashitha Nalaranga <nalaranga@gmail.com>
2026-09-07 18:35:16 +0530

    logo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,8 +31,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Health Check
 app.get('/api/health', (req: Request, res: Response) => {
